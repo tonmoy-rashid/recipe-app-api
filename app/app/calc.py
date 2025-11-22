f970ef7cba7d570f860914a64d95bb717b345961
tonmoy-rashid <tonmoyrashid1234@gmail.com>
2025-11-23 04:35:34 +0600

    Fixing the linting problem for calc.py and tests.py

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,10 +2,12 @@
 Calculator functions
 """
 
-def add(a,b):
+
+def add(a, b):
     """Add two numbers"""
     return a+b
 
-def subtract(a,b):
+
+def subtract(a, b):
     """Subtract a from b"""
     return b-a
--- app/app/tests.py
+++ app/app/tests.py
@@ -4,15 +4,16 @@ Sample tests
 from django.test import SimpleTestCase
 from app import calc
 
+
 class ClacTests(SimpleTestCase):
     """Test cases"""
 
     def test_add(self):
         """Test add function"""
-        res = calc.add(1,2)
-        self.assertEqual(res,3)
+        res = calc.add(1, 2)
+        self.assertEqual(res, 3)
 
     def test_subtract_numbers(self):
         """Subtract two numbers"""
-        res = calc.subtract(9,20)
-        self.assertEqual(res,11)
+        res = calc.subtract(9, 20)
+        self.assertEqual(res, 11)
